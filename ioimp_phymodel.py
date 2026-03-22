3a3f21b467b0682cc4058d557b3db4178e288689
GaleonD <indrajithgopinath@gmail.com>
2026-03-22 19:21:55 +0530

    Files updated.

---
+++ ioimp_phymodel.py
@@ -0,0 +1,31 @@
+import numpy as np
+from ioimp_datalib import dopant_data
+
+# the math
+
+def doping_profile(dopant, energy, dose):
+    if energy not in dopant_data(dopant):
+        energies = sorted(dopant_data[dopant].keys())
+        closest_energy = (min(energies, key=lambda x: abs(x - energy)))
+        rp, drp = dopant_data[dopant][closest_energy]
+    
+    else:
+        rp, drp = dopant_data[dopant][energy]
+
+    x = np.linspace(0, 1000, 500)
+
+    # formulas
+
+    cp = dose / (np.sqrt(2 * pi) * drp)
+    cx = cp * np.exp(-(x - rp)**2 / (2 * (drp)**2))
+
+    return x, cx
+
+# execute
+
+if __name__ = "__main__":
+    depth, conc = calculate = doping_profile(Boron, 100, 1e15)
+    print(f"Peak Concentration: {max(conc):.2e} atoms/cm^3")
+
+
+
